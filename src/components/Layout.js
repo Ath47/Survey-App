f93e88e4aecc292c58f625cd5ea5ed69cbc32769
Atharva K <atharva47kulkarni@gmail.com>
2024-03-24 14:31:50 +0530

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,9 @@
+import Header from "@/pages/Header";
+
+export default function Layout({children}) {
+    return (
+        <div className="w-5/6 bg-slate-200 justify-center mx-auto mt-10 h-[400px] rounded-lg p-2">
+        {children}
+        </div>
+    )
+}
